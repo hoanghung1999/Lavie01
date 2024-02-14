7060ffedd7380d7a8a3cbbeae42cb7becb23dd9a
unknown <hungcn8b@gmail.com>
2024-02-14 22:07:09 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "_Lavie.01",
+    introTitle: 'Valentine Day To Dieu Thu',
+    introDesc: `🥰🥰🥰🥰🥰
+    <33333333
+    😘😘😘😘😘
+    `,
+    btnIntro: 'Bình Yên bên cạnh 1 người ',
+    title: 'Hãy nói yêu mình nhé, mình đáng yêu mà 🥰',
+    desc: '',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Cảm ơn tớ đi cậu ôi^^',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: '^^',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/hoanghung.hero' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
